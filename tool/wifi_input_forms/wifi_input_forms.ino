91304a17753cc3d51fb8c18c84a1a0bad754210f
Antreas Christofi <victoryspecificationii@gmail.com>
2022-07-26 18:52:33 +0300

    attempt 2 at the web server thing

--- tool/wifi_input_forms/wifi_input_forms.ino
+++ tool/wifi_input_forms/wifi_input_forms.ino
@@ -8,75 +8,38 @@
 #endif
 #include <ESPAsyncWebServer.h>
 
-
-#include <Servo.h>
-#include "HCPCA9685.h"
-#include <Wire.h> //include Wire.h library
-
-
-/* I2C slave address for the device/module. For the HCMODU0097 the default I2C address
-   is 0x40 */
-#define  I2CAdd 0x40
- 
- 
-/* Create an instance of the library */
-HCPCA9685 HCPCA9685(I2CAdd);
-
-
-
-//Specify external pins
-int drivePin = D0;
-Servo driveMotor;
-
-
-
 AsyncWebServer server(80);
 
+// REPLACE WITH YOUR NETWORK CREDENTIALS
 const char* ssid = "To Hani Living";
 const char* password = "welcometohani";
 
-const char* input_throttle = "input_integer";
-const char* input_brake = "input_integer";
-const char* input_steering = "input_integer";
-const char* input_aero_f = "input_integer";
-const char* input_aero_r = "input_integer";
+const char* PARAM_INPUT_1 = "input1";
+const char* PARAM_INPUT_2 = "input2";
+const char* PARAM_INPUT_3 = "input3";
 
+int thr;
+int brk;
+int str;
 
-//server ui
+// HTML web page to handle 3 input fields (input1, input2, input3)
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML><html><head>
-  <title>refactored-octo-adventure UI</title>
+  <title>ESP Input Form</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <style>
-    html {font-family: Times New Roman; display: inline-block; text-align: center;}
-    h2 {font-size: 3.0rem; color: #FF0000;}
-  </style>
   </head><body>
-  <h2>refactored-octo-adventure | Main Controller UI</h2> 
-    <form action="/get">
-    Throttle | Integer | Input Range: 0 < x < 180 : <input type="text" name="input_integer">
+  <form action="/get">
+    input1: <input type="text" name="input1">
     <input type="submit" value="Submit">
   </form><br>
-    </form>
-    <form action="/get">
-    Brake | Integer | Input Range: 0 < x < 90 : <input type="text" name="input_integer">
+  <form action="/get">
+    input2: <input type="text" name="input2">
     <input type="submit" value="Submit">
   </form><br>
-    </form>
-    <form action="/get">
-    Steering | Integer | Input Range: -180 < x < 180 : <input type="text" name="input_integer">
+  <form action="/get">
+    input3: <input type="text" name="input3">
     <input type="submit" value="Submit">
-  </form><br>
-    </form>
-    <form action="/get">
-    Front Aero | Integer | Input Range: -45 < x < 45 : <input type="text" name="input_integer">
-    <input type="submit" value="Submit">
-  </form><br>
-    </form>
-    <form action="/get">
-    Rear Aero | Integer | Input Range: -45 < x < 45 : <input type="text" name="input_integer">
-    <input type="submit" value="Submit">
-  </form><br>
+  </form>
 </body></html>)rawliteral";
 
 void notFound(AsyncWebServerRequest *request) {
@@ -88,97 +51,61 @@ void setup() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connecting...");
+    Serial.println("WiFi Failed!");
     return;
   }
   Serial.println();
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
-
-
-  delay(2000);
-
-  //Attach drive and turn motors
-  driveMotor.attach(drivePin);
-  Serial.println("                                     ");
-  Serial.println("=================REMEMBER===============");
-  Serial.println("Throttle Input Range is 0 <= x <= 180");
-  Serial.println("Brake Input Range is 0 <= x <= 90");
-
-  //Neutral the drive motor
-  driveMotor.write(90);
-
-  /* Initialise the library and set it to 'servo mode' */ 
-  HCPCA9685.Init(SERVO_MODE);
-
-  Serial.println("                                        ");
-  Serial.println("=================REMEMBER===============");
-  Serial.println("Steering Input Range is -180 <= x <= 180");
-
-  Serial.println("                                        ");
-  Serial.println("=================REMEMBER===============");
-  Serial.println("Front Aero Input Range is -45 <= x <= 45");
-  Serial.println("Rear Aero Input Range is -45 <= x <= 45");
- 
-  /* Wake the device up */
-  HCPCA9685.Sleep(false);
-
-
-  //create server page
+  // Send web page with input fields to client
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/html", index_html);
   });
 
-/*
-  //process input from server
+  // Send a GET request to <ESP_IP>/get?input1=<inputMessage>
   server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    String input_message;
-    String input_parameter;
-    
-    if (request->hasParam(input_throttle)) {
-      input_message = request->getParam(input_throttle)->value();
-      input_parameter = input_throttle;
-     
+    String inputMessage;
+    String inputParam;
+    // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
+    if (request->hasParam(PARAM_INPUT_1)) {
+      inputMessage = request->getParam(PARAM_INPUT_1)->value();
+      inputParam = PARAM_INPUT_1;
+      thr=inputMessage.toInt();
+      Serial.print("Value is:");
+      Serial.println(thr);
     }
-
-    else if (request->hasParam(input_brake)) {
-      input_message = request->getParam(input_brake)->value();
-      input_parameter = input_brake;
+    // GET input2 value on <ESP_IP>/get?input2=<inputMessage>
+    else if (request->hasParam(PARAM_INPUT_2)) {
+      inputMessage = request->getParam(PARAM_INPUT_2)->value();
+      inputParam = PARAM_INPUT_2;
+      brk=inputMessage.toInt();
+      Serial.print("Value is:");
+      Serial.println(brk);
     }
-
-    else if (request->hasParam(input_steering)) {
-      input_message = request->getParam(input_steering)->value();
-      input_parameter = input_steering;
-    }
-
-    else if (request->hasParam(input_aero_f)) {
-      input_message = request->getParam(input_aero_f)->value();
-      input_parameter = input_aero_f;
-    }
-
-    else if (request->hasParam(input_aero_r)) {
-      input_message = request->getParam(input_aero_r)->value();
-      input_parameter = input_aero_r;
+    // GET input3 value on <ESP_IP>/get?input3=<inputMessage>
+    else if (request->hasParam(PARAM_INPUT_3)) {
+      inputMessage = request->getParam(PARAM_INPUT_3)->value();
+      inputParam = PARAM_INPUT_3;
+      str=inputMessage.toInt();
+      Serial.print("Value is:");
+      Serial.println(str);
     }
     else {
-      input_message = "No message sent";
-      input_parameter = "none";
+      inputMessage = "No message sent";
+      inputParam = "none";
     }
-    Serial.print("Value is: ");
-    Serial.println(input_message);
-    request->send(200, "text/html", "HTTP GET request sent to your ESP on input field ("+ input_parameter + ") with value: " + input_message + "<br><a href=\"/\">Return to Home Page</a>");
-    driveMotor.write(input_message.toInt());   
-
-    
+    Serial.println(inputMessage);
+    request->send(200, "text/html", "HTTP GET request sent to your ESP on input field (" 
+                                     + inputParam + ") with value: " + inputMessage +
+                                     "<br><a href=\"/\">Return to Home Page</a>");
   });
-  */
+
   server.onNotFound(notFound);
   server.begin();
 }
 
+
 void loop() {
-
-
-
+  
 }
